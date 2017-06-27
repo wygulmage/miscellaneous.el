a09a3e994aa0c4c01b38f5330b2c0a7132cce331
Keith Wygant <kawygant@ncsu.edu>
2017-06-27 11:19:04 -0400

    added requires.

--- miscellaneous.el
+++ miscellaneous.el
@@ -1,6 +1,9 @@
 ;;; miscellaneous.el --- random crap that belongs somewhere else -*- lexical-binding: t -*-
 
 ;;; Some convenience functions that don't belong here but I don' have a better place to put them.
+(eval-when-compile
+  (require 'cl-loop))
+(require 'seq)
 
 (defun misc--princ (OBJECT &optional PRINT-CHAR-FUNCTION)
   "`princ' but does not print the colon of a keyword"
