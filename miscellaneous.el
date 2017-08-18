53a9ce50bb4c303cf6dd108589b19a4b7caa1cdb
Keith Wygant <keith.wygant@gmail.com>
2017-08-17 23:02:52 -0400

    added an oroboros procedure.

--- miscellaneous.el
+++ miscellaneous.el
@@ -5,6 +5,8 @@
   (require 'cl-macs))
 (require 'seq)
 
+;;; Strings and Symbols:
+
 (defun misc--princ (OBJECT &optional PRINT-CHAR-FUNCTION)
   "`princ' but does not print the colon of a keyword"
   (princ (if (keywordp OBJECT)
@@ -29,11 +31,61 @@ Example: (misc--pad 5 \"Hi!\") => \"  Hi!\""
   (declare (pure t) (side-effect-free t))
   (format (concat "%" (number-to-string W) "s") S))
 
+
+;;; Lists:
+
 (defun misc--alternate (LIST1 LIST2)
   "Create a list that alternates the elements of LIST1 and LIST2."
   (cl-loop
    for e1 in LIST1 and e2 in LIST2
    append (list e1 e2)))
+
+
+(defun misc/folder (FUNCTION IDENTITY)
+  ":: (a -> b -> b) -> b ->
+     (Maybe (a . b) -> b)"
+  (lambda (maybe)
+    (pcase maybe
+      (`(,a . ,b) (funcall FUNCTION a b))
+      ('() IDENTITY))))
+
+;; (defun misc/fold-rec (LIST FOLDER)
+;;   ":: [a] -> (Maybe (a . b) -> b) -> b"
+;;   (funcall FOLDER
+;;    (pcase LIST
+;;      (`(,x . ,xx)
+;;       `(,x . ,(misc/fold-rec xx FOLDER))))))
+
+;; (defun misc/unfold-rec (SEED UNFOLDER)
+;;   ":: s -> (s -> Maybe (x . s)) -> s"
+;;   (pcase (funcall UNFOLDER SEED)
+;;     (`(,x . ,state)
+;;      `(,x . ,(misc/unfold-rec state UNFOLDER)))))
+
+;; (defun misc/refold-rec (SEED UNFOLDER FOLDER)
+;;   ":: s -> (s -> Maybe (a . s)) -> (Maybe (a . b) -> b) -> b
+;; This is a left-fold."
+;;   (funcall FOLDER
+;;            (pcase (funcall UNFOLDER SEED)
+;;              (`(,x . ,state)
+;;               `(,x . ,(misc/refold-rec state UNFOLDER FOLDER))))))
+
+(defun misc/refold (FOLDER UNFOLDER SEED)
+  "Hatch a snake, feed it its own tail, and return what's left.
+:: (Maybe (a . b) -> b) -> (s -> Maybe (a . s)) -> s -> b
+Reverses order of application."
+  (let ((result (funcall FOLDER ()))
+        (maybe  (funcall UNFOLDER SEED)))
+    (while maybe
+      (setq result (funcall FOLDER   (cons (car maybe) result))
+            maybe  (funcall UNFOLDER (cdr maybe))))
+    result))
+
+(defun misc/fold (FUNCTION IDENTITY LIST)
+  (misc/refold (misc/folder FUNCTION IDENTITY) #'identity LIST))
+
+(defun misc/unfold (UNFOLDER SEED)
+  (misc/refold #'identity UNFOLDER SEED))
 
 ;;; Numbers:
 
