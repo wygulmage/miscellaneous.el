f55773d1cf97f7013847c3316736c83d4dc94709
Keith Wygant <kawygant@ncsu.edu>
2018-02-13 10:20:12 -0500

    added purity declarations, some bug fixes.

--- miscellaneous.el
+++ miscellaneous.el
@@ -3,7 +3,7 @@
 ;;; Some convenience functions that don't belong here but I don't have a better place to put them.
 (eval-when-compile
   (require 'cl-macs))
-(require 'seq)
+(require 'seq) ; `seq-doseq', `seq-partition'
 
 ;;; Strings and Symbols:
 
@@ -16,6 +16,7 @@
 
 (defun misc--mkstr (&rest ARGS)
   "Create a string from arbitrary arguments."
+  (declare (pure t) (side-effect-free t))
   (with-output-to-string
     (mapc #'misc--princ ARGS)))
 
@@ -35,6 +36,7 @@ Example: (misc--pad 5 \"Hi!\") => \"  Hi!\""
 
 (defun misc--alternate (LIST1 LIST2)
   "Create a list that alternates the elements of LIST1 and LIST2."
+  (declare (pure t) (side-effect-free t))
   (cl-loop
    for e1 in LIST1 and e2 in LIST2
    append (list e1 e2)))
@@ -48,31 +50,54 @@ Example: (misc--pad 5 \"Hi!\") => \"  Hi!\""
       (`(,a . ,b) (funcall FUNCTION a b))
       ('() IDENTITY))))
 
+;; This takes advantage of the fact that (a . [a]) :: [a].
+;; One possible process:
+;; 1. Pass seed0 to the unfolder.
+;; 2. The unfolder produces a Maybe (value1, seed1).
+;; 3. Pass the Maybe (value1, seed1) to the folder.
+;; 4. The folder produces a
+
 ;; (defun misc/fold-rec (LIST FOLDER)
-;;   ":: [a] -> (Maybe (a . b) -> b) -> b"
+;;   ":: [a] -> (Maybe (a . s) -> s) -> s"
 ;;   (funcall FOLDER
 ;;    (pcase LIST
 ;;      (`(,x . ,xx)
 ;;       `(,x . ,(misc/fold-rec xx FOLDER))))))
 
 ;; (defun misc/unfold-rec (SEED UNFOLDER)
-;;   ":: s -> (s -> Maybe (x . s)) -> s"
+;;   ":: s -> (s -> Maybe (a . s)) -> [a]"
 ;;   (pcase (funcall UNFOLDER SEED)
 ;;     (`(,x . ,state)
 ;;      `(,x . ,(misc/unfold-rec state UNFOLDER)))))
 
-;; (defun misc/refold-rec (SEED UNFOLDER FOLDER)
-;;   ":: s -> (s -> Maybe (a . s)) -> (Maybe (a . b) -> b) -> b
-;; This is a left-fold."
-;;   (funcall FOLDER
-;;            (pcase (funcall UNFOLDER SEED)
-;;              (`(,x . ,state)
-;;               `(,x . ,(misc/refold-rec state UNFOLDER FOLDER))))))
+(defun misc/refold (SEED UNFOLDER FOLDER)
+  ":: s -> (s -> Maybe (a . s)) -> (Maybe (a . b) -> b) -> b
+This is a left-fold."
+  (funcall FOLDER
+           (pcase (funcall UNFOLDER SEED)
+             (`(,x . ,state)
+              `(,x . ,(misc/refold state UNFOLDER FOLDER)))
+             ('() ()))))
 
-(defun misc/refold (FOLDER UNFOLDER SEED)
+;; ;; Canonical:
+;; (defun misc/fold-rec (FOLDER LIST)
+;;   ":: (Maybe (a . b) -> b) -> [a] -> b"
+;;   (funcall FOLDER
+;;    (pcase LIST
+;;      (`(,x . ,xx)
+;;       `(,x . ,(misc/fold-rec FOLDER xx))))))
+
+;; (defun misc/unfold-rec (UNFOLDER SEED)
+;;   ":: (b -> Maybe (a . b)) -> b -> [a]"
+;;   (pcase (funcall UNFOLDER SEED)
+;;     (`(,x . ,new-seed)
+;;      `(,x . ,(misc/unfold-rec UNFOLDER new-seed)))))
+
+
+(defun misc/refold-left (FOLDER UNFOLDER SEED)
   "Hatch a snake, feed it its own tail, and return what's left.
 :: (Maybe (a . b) -> b) -> (s -> Maybe (a . s)) -> s -> b
-Reverses order of application."
+This is a left fold."
   (let ((result (funcall FOLDER ()))
         (maybe  (funcall UNFOLDER SEED)))
     (while maybe
@@ -80,11 +105,21 @@ Reverses order of application."
             maybe  (funcall UNFOLDER (cdr maybe))))
     result))
 
-(defun misc/fold (FUNCTION IDENTITY LIST)
-  (misc/refold (misc/folder FUNCTION IDENTITY) #'identity LIST))
-
 (defun misc/unfold (UNFOLDER SEED)
   (misc/refold #'identity UNFOLDER SEED))
+
+(defun misc/fold (FUNCTION IDENTITY LIST)
+  "bog standard left-fold function"
+  (misc/refold (misc/folder FUNCTION IDENTITY) #'identity LIST))
+
+(defun misc//fix (f) (lambda (x) (funcall (funcall f (misc//fix f)) x)))
+
+;; (defalias 'misc/left #'car)
+;; (defalias 'misc/right #'cdr)
+;; (defalias 'misc/branch? #'consp)
+;; (defalias 'misc/leaf? #'vectorp)
+;; (defun misc/treefold (FOLDER LIST)
+;;   ":: (Either a (b, b) -> b) -> Tree a -> b")
 
 ;;; Numbers:
 
@@ -95,15 +130,15 @@ Example: (misc--digits 10.7) => 4"
   (declare (pure t) (side-effect-free t))
   (length (number-to-string N)))
 
-;;; Key maps
+;;; Key maps:
 
 (defun misc--def-keys (MAP &rest BINDINGS)
   "Create new key bindings in MAP.
 Each binding should be a string that can be passed to `kbd' followed by an interactive procedure."
   (declare (indent defun))
   (seq-doseq (b (seq-partition BINDINGS 2))
-    (define-key MAP (kbd (seq-elt b 0))
-      (seq-elt b 1))))
+    (define-key MAP (kbd (elt b 0))
+      (elt b 1))))
 
 
 (provide 'miscellaneous)
